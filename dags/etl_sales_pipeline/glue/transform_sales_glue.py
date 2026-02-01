93e02048a7ab15e6eba93e9c5b0459c92ee91ccc
nag1045 <vishwkarmanagendra@gmail.com>
2026-02-02 00:04:25 +0530

    made some minor changes before running on airflow

--- dags/etl_sales_pipeline/glue/transform_sales_glue.py
+++ dags/etl_sales_pipeline/glue/transform_sales_glue.py
@@ -2,30 +2,72 @@ import sys
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.job import Job
 from pyspark.sql.functions import col, to_date
 
-# Get job arguments
-args = getResolvedOptions(sys.argv, ["execution_date"])
+# --------------------------------------------------
+# Arguments (from Airflow / Glue job parameters)
+# --------------------------------------------------
+args = getResolvedOptions(
+    sys.argv,
+    ["JOB_NAME", "execution_date"]
+)
+
 execution_date = args["execution_date"]
 
+# --------------------------------------------------
+# Spark / Glue Context
+# --------------------------------------------------
 sc = SparkContext()
-glueContext = GlueContext(sc)   
-spark = glueContext.spark_session
+glue_context = GlueContext(sc)
+spark = glue_context.spark_session
 
-input_path = f"s3://nag-sales-data-bucket/landing/sales/{execution_date}/"
-output_path = f"s3://nag-sales-data-bucket/curated/sales/{execution_date}/"
+job = Job(glue_context)
+job.init(args["JOB_NAME"], args)
 
-# Read raw data
-df = spark.read.option("header", "true").csv(input_path)
+# --------------------------------------------------
+# S3 Paths
+# --------------------------------------------------
+input_path = (
+    f"s3://nag-sales-data-bucket/landing/sales/"
+    f"{execution_date}/sales.csv"
+)
 
-# Transform (ETL logic)
+output_path = (
+    f"s3://nag-sales-data-bucket/curated/sales/"
+    f"{execution_date}/"
+)
+
+# --------------------------------------------------
+# Read raw CSV
+# --------------------------------------------------
+df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(input_path)
+)
+
+# --------------------------------------------------
+# Transformations
+# --------------------------------------------------
 df_clean = (
     df
     .filter(col("order_id").isNotNull())
-    .filter(col("amount").isNotNull())
-    .withColumn("amount", col("amount").cast("decimal(10,2)"))
+    .filter(col("total_amount").isNotNull())
+    .withColumn("total_amount", col("total_amount").cast("decimal(12,2)"))
     .withColumn("order_date", to_date(col("order_date")))
 )
 
-# Write curated data
-df_clean.write.mode("overwrite").parquet(output_path)
+# print(f"Row count = {df_clean.count()}")
+
+# --------------------------------------------------
+# Write curated Parquet
+# --------------------------------------------------
+(
+    df_clean.write
+    .mode("overwrite")
+    .parquet(output_path)
+)
+
+job.commit()

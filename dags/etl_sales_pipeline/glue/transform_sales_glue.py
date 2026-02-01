a19440da82dd0528b87f3b0df963a60ec47333ea
nag1045 <vishwkarmanagendra@gmail.com>
2026-02-02 00:33:56 +0530

    added static path to input and output

--- dags/etl_sales_pipeline/glue/transform_sales_glue.py
+++ dags/etl_sales_pipeline/glue/transform_sales_glue.py
@@ -1,45 +1,26 @@
-import sys
-from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.sql.functions import col, to_date
 
 # --------------------------------------------------
-# Arguments (from Airflow / Glue job parameters)
-# --------------------------------------------------
-args = getResolvedOptions(
-    sys.argv,
-    ["JOB_NAME", "execution_date"]
-)
-
-execution_date = args["execution_date"]
-
-# --------------------------------------------------
-# Spark / Glue Context
+# Glue / Spark Context
 # --------------------------------------------------
 sc = SparkContext()
 glue_context = GlueContext(sc)
 spark = glue_context.spark_session
 
 job = Job(glue_context)
-job.init(args["JOB_NAME"], args)
+job.init("sales-etl-glue-job", {})   # Job name can be static
 
 # --------------------------------------------------
-# S3 Paths
+# Hard-coded S3 paths
 # --------------------------------------------------
-input_path = (
-    f"s3://nag-sales-data-bucket/landing/sales/"
-    f"{execution_date}/sales.csv"
-)
-
-output_path = (
-    f"s3://nag-sales-data-bucket/curated/sales/"
-    f"{execution_date}/"
-)
+input_path = "s3://nag-sales-data-bucket/landing/sales/sales.csv"
+output_path = "s3://nag-sales-data-bucket/curated/sales/"
 
 # --------------------------------------------------
-# Read raw CSV
+# Read CSV
 # --------------------------------------------------
 df = (
     spark.read
@@ -49,25 +30,20 @@ df = (
 )
 
 # --------------------------------------------------
-# Transformations
+# Transform
 # --------------------------------------------------
 df_clean = (
     df
     .filter(col("order_id").isNotNull())
-    .filter(col("total_amount").isNotNull())
-    .withColumn("total_amount", col("total_amount").cast("decimal(12,2)"))
     .withColumn("order_date", to_date(col("order_date")))
 )
 
-# print(f"Row count = {df_clean.count()}")
+# Force Spark execution (good for debugging)
+print(f"Row count = {df_clean.count()}")
 
 # --------------------------------------------------
-# Write curated Parquet
+# Write Parquet
 # --------------------------------------------------
-(
-    df_clean.write
-    .mode("overwrite")
-    .parquet(output_path)
-)
+df_clean.write.mode("overwrite").parquet(output_path)
 
 job.commit()
--- dags/etl_sales_pipeline/sql/copy_sales.sql
+++ dags/etl_sales_pipeline/sql/copy_sales.sql
@@ -1,4 +1,4 @@
 COPY sales_clean
-FROM 's3://nag-sales-data-bucket/curated/sales/{{ ds }}/'
+FROM 's3://nag-sales-data-bucket/curated/sales/'
 IAM_ROLE 'arn:aws:iam::467866449044:role/service-role/AmazonRedshift-CommandsAccessRole-20260201T205546'
 FORMAT AS PARQUET;

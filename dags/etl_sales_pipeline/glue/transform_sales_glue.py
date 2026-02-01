220725a22296b2f2e7917dc1f4a2e5903afe1a17
nag1045 <vishwkarmanagendra@gmail.com>
2026-02-01 16:06:32 +0530

    added real dag for sales data transformation

---
+++ dags/etl_sales_pipeline/glue/transform_sales_glue.py
@@ -0,0 +1,31 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql.functions import col, to_date
+
+# Get job arguments
+args = getResolvedOptions(sys.argv, ["execution_date"])
+execution_date = args["execution_date"]
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+input_path = f"s3://nag-sales-data-bucket/landing/sales/{execution_date}/"
+output_path = f"s3://nag-sales-data-bucket/curated/sales/{execution_date}/"
+
+# Read raw data
+df = spark.read.option("header", "true").csv(input_path)
+
+# Transform (ETL logic)
+df_clean = (
+    df
+    .filter(col("order_id").isNotNull())
+    .filter(col("amount").isNotNull())
+    .withColumn("amount", col("amount").cast("decimal(10,2)"))
+    .withColumn("order_date", to_date(col("order_date")))
+)
+
+# Write curated data
+df_clean.write.mode("overwrite").parquet(output_path)
